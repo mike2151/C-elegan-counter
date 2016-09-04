daedbf9b0deeabf8c1c174ccdd39e3bee2fc49fe
mike2151 <redefiningproductions@gmail.com>
2016-09-03 22:47:30 -0400

    picture analysis and measuring added

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 
 app.disable('x-powered-by');
 
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,109 @@ var fs = require('fs');
 var Caman = require('caman').Caman;
 var Jimp = require("jimp");
 var cv = require("opencv");
+var multer  =   require('multer');
+
+
+//generate unique token for the session
+var session_token = getRandomInt(1, 10000000).toString();
+
+//declared to pass data to analysis
+var image_width;
+var image_height;
+var worm_array;
+
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + session_token + ".png");
+  }
+});
+
+var upload = multer({ storage : storage}).single('photo');
 
 
 router.get("/", function(req,res) {
     res.render("index");
-    Caman("./public/images/main.jpeg", function () {
-        this.contrast(65);
-        this.hue(100);
-        this.vibrance(100);
-        this.greyscale();
-        //this.exposure(50);
-        this.render(function () {
-            this.save("./public/images/output.png");
-            processWorms();
-            find_contours();
-  });
+    
+});
+
+router.get("/analyze", function(req,res) {
+    res.render("analyze");
+});
+
+router.get("/measure", function(req,res) {
+    res.render("draw");
+});
+
+
+router.post("/analyze", function(req,res) {
+    upload(req,res,function(err) {
+        if(err) {
+            res.redirect("/analyze");
+        }
+        //console.log("photo-" + session_token + ".png");
+        var area_percent = req.body.area_percent;
+
+        initial_process();
+        while(!fileExists("./public/uploads/" + "output-" + session_token + ".png")) {require('deasync').sleep(1000);}
+        processWorms();
+        while(!fileExists("./public/uploads/" + "processed-" + session_token + ".png")) {require('deasync').sleep(1000);}
+        find_contours(area_percent);
+        
+        res.redirect("/analyze/" + session_token);
+    });
+});
+
+router.get("/analyze/:token", function(req,res) {
+    var token = req.params.token;
+    if (fileExists("./public/uploads/" + "photo-" + token + ".png")){fs.unlinkSync("./public/uploads/" + "photo-" + token + ".png");}
+    if (fileExists("./public/uploads/" + "output-" + token + ".png")){fs.unlinkSync("./public/uploads/" + "output-" + token + ".png");}
+    if (fileExists("./public/uploads/" + "processed-" + token + ".png")){fs.unlinkSync("./public/uploads/" + "processed-" + token + ".png");}
+
+    var image_link = ("../uploads/" + "worms-" + token + ".png");
+
+    res.render("analyze_image", {image_link: image_link, image_width: image_width, image_height: image_height, worm_array: worm_array});
+});
+
+
+router.get("/video", function(req,res) {
+    res.render("video_analyze");
+});
+
+router.post("/video", function(req,res) {
+    
 });
 
 
 
-});
+
+
+
+
+
+
+
+
+function initial_process() {
+    
+    Caman("./public/uploads/" + "photo-" + session_token + ".png", function () {
+                this.contrast(65);
+                this.hue(100);
+                this.vibrance(100);
+                this.greyscale();
+                this.render(function () {
+                    this.save("./public/uploads/" + "output-" + session_token + ".png");
+                });
+            });
+    
+}
 
 function processWorms() {
-    Jimp.read("./public/images/output.png").then(function (pic) {
-
+    
+    Jimp.read("./public/uploads/" + "output-" + session_token + ".png").then(function (pic) {
+        
         
     pic.scan(0, 0, pic.bitmap.width, pic.bitmap.height, function (x, y, idx) {
              var red   = this.bitmap.data[ idx + 0 ];
@@ -66,6 +145,7 @@ function processWorms() {
 
                  
 
+
                  if (bottom_is_white || right_is_white) {
                      this.bitmap.data[idx] = 255;
                      this.bitmap.data[idx + 1] = 255;
@@ -75,29 +155,39 @@ function processWorms() {
 
     });
         }
-
-         pic.write("./public/images/processed.png"); // save 
+        
+         pic.write("./public/uploads/" + "processed-" + session_token + ".png"); // save 
 }).catch(function (err) {
     console.error(err);
 });
 
 }
 
-function find_contours() {
+function find_contours(area_percent) {
+    
 
-  cv.readImage("./public/images/processed.png", function(err, im){
+  cv.readImage("./public/uploads/" + "processed-" + session_token + ".png", function(err, im){
       
     if (err) throw err;
-
-    var maxArea = 2500;
+    worm_array = [];
+    var width = im.width();
+    var height = im.height();
+    image_width = im.width();
+    image_height = im.height();
+    var total_area = width*height;
+    var maxArea;
+    if (!area_percent) {maxArea = 2500;}
+    else {
+     maxArea = parseFloat(total_area) * area_percent;
+    }
+   
     var GREEN = [0, 255, 0]; // B, G, R
     var WHITE = [255, 255, 255]; // B, G, R
     var RED   = [0, 0, 255]; // B, G, R
 
     var worm_contours = [];
 
-    var width = im.width();
-    var height = im.height();
+    
 
     var big = new cv.Matrix(height, width);
     var all = new cv.Matrix(height, width);
@@ -112,7 +202,6 @@ function find_contours() {
     for(i = 0; i < contours.size(); i++) {
     if(contours.area(i) > maxArea) {
 
-//see if width and height of box are nearly equal 
 
       worm_contours.push(i);
       var moments = contours.moments(i);
@@ -125,23 +214,46 @@ function find_contours() {
       var point1 = [array_of_points["x"], array_of_points["y"]];
       var point2 = [array_of_points["width"], array_of_points["height"]]
       big.rectangle(point1, point2, WHITE);
-      array_of_points = [];
+      
 
       
-      big.putText(worm_contours.length.toString() + ": " + Number((contours.arcLength(i)).toFixed(1)) + "px", cgx, cgy, "HERSEY_COMPLEX_SMALL", RED, 1, 1);
-  
+      //arclength is the perimeter
+      big.putText(worm_contours.length.toString(), cgx, cgy, "HERSEY_COMPLEX_SMALL", RED, 1, 1);
+
+      var array_len = Number((contours.arcLength(i)/2.0).toFixed(1));
+      var array_area = Number((contours.area(i)).toFixed(1));
+      var array_width = array_of_points["width"];
+      var array_height = array_of_points["height"];
+
+
+     worm_array.push({len: array_len, area: array_area, bounding_width: array_width, bounding_height: array_height});   
+     array_of_points = [];
     }
   }
-/** 
-  console.log("size:" + worm_contours.length.toString());
-  for (var j = 0; j < worm_contours.length; j++) {
-      console.log(contours.area(worm_contours[j], true));
-  }
-  */
-  big.save('./public/images/worms.png');
-  
-  
+
+
+
+  big.save("./public/uploads/" + "worms-" + session_token + ".png");
 });
+}
+
+
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function fileExists(filePath)
+{
+    try
+    {
+        return fs.statSync(filePath).isFile();
+    }
+    catch (err)
+    {
+        return false;
+    }
 }
 
 
